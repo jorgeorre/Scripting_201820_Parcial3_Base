1630b8f1a6d85a6ec3e68fdf6b7a99d2e375d42b
Jorge Orrego <36045304+jorgeorre@users.noreply.github.com>
2018-10-02 07:36:50 -0500

    Add files via upload

---
+++ Scripts/BTBase/Selector.cs
@@ -0,0 +1,40 @@
+﻿public class Selector : Composite
+{
+    protected override bool MustAllChildrenSucceed
+    {
+        get
+        {
+            return false;
+        }
+    }
+
+    public override bool Execute()
+    {
+        bool result = CheckCondition();
+
+        if (result)
+        {
+            foreach (Node node in children)
+            {
+                result = result || node.Execute();
+
+                if (ShouldBreak(result))
+                {
+                    break;
+                }
+            }
+        }
+
+        return result;
+    }
+
+    protected override bool ShouldBreak(bool result)
+    {
+        return result;
+    }
+
+    protected virtual bool CheckCondition()
+    {
+        return true;
+    }
+}
---
+++ Scripts/UIManager.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class UIManager : MonoBehaviour
+{
+    [SerializeField]
+    private Text timerText;
+    private GameController time;
+
+    // Use this for initialization
+    private void Start()
+    {
+        if (timerText == null)
+        {
+            enabled = false;
+        }
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        
+    }
+}
